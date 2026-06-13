4ab560259eba8628fe97c6a3331fccc67ab6ef7d
Sky1sBloo <ultimate8gaming@gmail.com>
2026-06-13 22:43:12 +0800

    Removed reserved port

--- api.py
+++ api.py
@@ -207,4 +207,4 @@ if __name__ == "__main__":
     print("  Flask backend running")
     print("  Open:  http://localhost:1000")
     print("=" * 50 + "\n")
-    app.run(host="0.0.0.0", debug=True, port=1000)
+    app.run(host="0.0.0.0", debug=True, port=10000)
